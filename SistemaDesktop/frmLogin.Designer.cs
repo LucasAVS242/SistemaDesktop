a4fe084553e22cf010ff760db2c6bead5842f53f
LucasAVS242 <lucasventurini242@outlook.com>
2023-06-09 20:04:03 -0300

    c

--- SistemaDesktop/frmLogin.Designer.cs
+++ SistemaDesktop/frmLogin.Designer.cs
@@ -142,6 +142,7 @@ namespace SistemaDesktop
             this.Controls.Add(this.lblSenha);
             this.Controls.Add(this.lblUsuario);
             this.Name = "frmLogin";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Login";
             this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.frmLogin_FormClosed);
             this.Load += new System.EventHandler(this.frmLogin_Load);
